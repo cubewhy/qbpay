cf0d15187aa8ac0f97fde89c42a64e1e920ba9e5
cubewhy <cubewhy@163.com>
2024-08-17 22:37:27 +0800

    feat: update script

---
+++ upgrade.sh
@@ -0,0 +1,8 @@
+# Auto update qbpay
+# Make sure you have git and jdk 17 installed
+echo "Fetching..."
+git pull
+echo "Building from source..."
+echo "You must have maven installed."
+bash mvn package
+echo "Done."
